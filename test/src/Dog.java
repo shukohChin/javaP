a8f1d738479b72ad732fe8bcfe7ffb0139fb0078
shukohChin <shukoh.chin@gmail.com>
2013-11-07 22:23:15 +0900

    test

---
+++ test/src/Dog.java
@@ -0,0 +1,10 @@
+
+public class Dog extends Animal {
+
+	protected String name = "dog";
+
+	public void eat(Animal a){
+		System.out.println(a.name);
+	}
+
+}
---
+++ test/src/Person.java
@@ -0,0 +1,10 @@
+
+public class Person extends Animal {
+
+	protected String name = "person";
+
+	public void eat(Animal a){
+		System.out.println(a.name);
+	}
+
+}
